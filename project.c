fa4fb2f301474cfd8705fcc6989661879e77863f
Vince <ver32@nau.edu>
2020-04-15 22:42:22 -0700

    added mpi stuff

--- project.c
+++ project.c
@@ -1,11 +1,15 @@
 #include <stdio.h>
+#include <mpi.h>
 
 
-int main(void)
+int main(int argc, char **argv)
 {
-  printf("RIP\n"); 
+  int my_rank, nprocs;
+  MPI_Init(&argc, &argv);
+  MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+  MPI_Comm_size(MPI_COMM_WORLD, &nprocs);
 
 
-
-  return 0; 
+  MPI_Finalize();
+  return 0;
 }
