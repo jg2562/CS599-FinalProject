efcae3719fabdce10e88966d32418415177cc2c3
Jack Garrard <jg2562@nau.edu>
2020-04-16 23:30:05 -0700

    Created conditions for the cells

---
+++ condition.c
@@ -0,0 +1,9 @@
+#include "condition.h"
+
+Condition createCondition(){
+	Condition condition;
+
+	condition.infection_probabilty = 0;
+
+	return condition;
+}
---
+++ condition.h
@@ -0,0 +1,10 @@
+#ifndef CONDITIONS_H
+#define CONDITIONS_H
+
+typedef struct Condition{
+	double infection_probabilty;
+} Condition;
+
+Condition createCondition();
+
+#endif /* CONDITIONS_H */
