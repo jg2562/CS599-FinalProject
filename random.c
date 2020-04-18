63a9ceaf8c31c384924c17e119434ec2b5e0c0c4
Jack Garrard <jg2562@nau.edu>
2020-04-17 19:14:16 -0700

    Added new random library

---
+++ random.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+
+void randomSeed(unsigned long seed){
+	srand(seed);
+}
+
+unsigned int getMaxRandomValue(){
+	return RAND_MAX;
+}
+
+unsigned int randomValue(){
+	return rand();
+}
+
+unsigned int randomInt(unsigned int max){
+	return randomValue() % max;
+}
+
+int randomUniformEvent(double probability){
+	double threshold = min(probability, 1) * RAND_MAX;
+	double event = rand();
+	return event < threshold;
+}
---
+++ random.h
@@ -0,0 +1,9 @@
+#ifndef RANDOM_H
+#define RANDOM_H
+
+void randomSeed(unsigned long seed);
+
+unsigned int randomInt(unsigned int max);
+int randomUniformEvent(double probability);
+
+#endif /* RANDOM_H */
