e90961d328106b167a10e5c45d6bd8018563b3fa
Jack Garrard <jg2562@nau.edu>
2020-04-27 21:41:03 -0700

    Added a duplicateString to utils

--- utils.c
+++ utils.c
@@ -1,6 +1,7 @@
 #include "utils.h"
 #include <stdlib.h>
 #include <ctype.h>
+#include <string.h>
 
 void lowerCaseString(char* str){
 	if (str == NULL){
@@ -20,4 +21,16 @@ double max(double a, double b){
 
 double min(double a, double b){
 	return a < b ? a : b;
+}
+
+char* duplicateString(const char* str){
+	if (str == NULL){
+		return NULL;
+	}
+
+	unsigned int len = strlen(str);
+	char* copy = malloc(sizeof(*copy) * len+1);
+	copy[len] = '\0';
+	strcpy(copy, str);
+	return copy;
 }
--- utils.h
+++ utils.h
@@ -4,5 +4,6 @@
 void lowerCaseString(char* str);
 double max(double a, double b);
 double min(double a, double b);
+char* duplicateString(const char* str);
 
 #endif /* UTILS_H */
